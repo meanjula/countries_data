a1349130cce1b9e7e18299820d7bd9876c2a372e
meanjula <anjula.ghale@gmail.com>
2021-11-22 21:11:43 +0200

    styled the page and fixed the readme file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   componentDidMount() {
     axios.get("https://restcountries.com/v3.1/all").then((res) => {
       this.setState({ data: res.data });
-      this.state.data.map((country) => this.state.data);
+      this.state.data.map((country) => console.log(this.state.data));
     });
   }
 
@@ -17,9 +17,16 @@ class App extends Component {
     return (
       <div className="countries">
         {this.state.data.map((country) => (
-          <div key={country.name.common} className="country">
+          <div key={country.name.common.toUpperCase()} className="country">
             <h1>{country.name.common}</h1>
-            <h2>{country.capital}</h2>
+            <h2>Capital: {country.capital}</h2>
+            {/* <h3>
+              {country.languages.length > 1 ? `Languages` : `Language`}:
+              {country.languages.map((language) => language).join(", ")}
+            </h3> */}
+            {/* <p>{country.currencies.map((cur) => cur.AFN.name)}</p> */}
+            <p>Population:{country.population}</p>
+
             <img src={country.flags.png} alt="country flag" />
           </div>
         ))}
