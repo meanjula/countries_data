6ad5eb272379649d1c6d52e5a177d28b654be951
meanjula <anjula.ghale@gmail.com>
2021-11-30 12:47:47 +0200

    added weather icon

--- src/components/country/CountrySingle.js
+++ src/components/country/CountrySingle.js
@@ -15,25 +15,59 @@ function getCountry(capital) {
 // }
 function getWeather(capital) {
   return axios.get(
-    `api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
   );
 }
 class CountrySingle extends Component {
   state = {
     country: [],
     weather: [],
+    isLoading: true,
   };
   //consolelog in componentdidmount for seeing what data is passed
   componentDidMount() {
-    console.log(this.props.params);
+    Promise.all([
+      getCountry(this.props.params.name),
+      getWeather(this.props.params.name),
+    ]).then((res) => {
+      this.setState({
+        country: res[0].data[0],
+        weather: res[1].data,
+        isLoading: false,
+      });
+      console.log("response", res);
+      console.log(this.state.country);
+      console.log("weather", this.state.weather);
+    });
   }
   render() {
-    return (
-      <div>
-        <h1>{this.props.params.name}</h1>
-        <p>Right now it is degree in{this.props.params.name}.</p>
-      </div>
-    );
+    if (this.state.isLoading) {
+      if (this.state.isLoading) {
+        return (
+          <div className="loader">
+            <div></div>
+            <div></div>
+          </div>
+        );
+      }
+    }
+    if (!this.state.isLoading) {
+      return (
+        <div>
+          <h1>{this.state.country.capital}</h1>
+          <p>
+            Right now it is {this.state.weather.main.temp}degree in{" "}
+            {this.state.country.capital}
+            {/* {this.state.weather.main.weather.main.temp}. */}
+          </p>
+          <img
+            src={`http://openweathermap.org/img/wn/${this.state.weather.weather[0].icon}@2x.png`}
+            alt={this.state.country.capital}
+            className="weather-img"
+          />
+        </div>
+      );
+    }
   }
 }
 
